d0b30d6ff58da2ea7f4143eab6d0a187b8fcbc19
Brian Lilly <brian@crystalfontz.com>
2014-03-25 12:41:16 -0700

    changed script to automatically execute the right commandline based on the devicetree

--- recipes/cfa-prebuilt/cfa-prebuilt/doom.sh
+++ recipes/cfa-prebuilt/cfa-prebuilt/doom.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
 export SDL_VIDEODRIVER=directfb
-/usr/bin/nice -n -20 /usr/bin/prboom -width 800 -height 480
+#set DOOM_CMDLINE = "/usr/bin/nice -n -20 /usr/bin/prboom -width 800 -height 480"
+
+read CMDLINE < /proc/device-tree/model
+for x in $CMDLINE; do
+    case $x in
+    CFA-10049)
+		/usr/bin/nice -n -20 /usr/bin/prboom -width 320 -height 480
+        ;;
+    CFA-10055)
+		/bin/nice -n -20 /usr/bin/prboom -width 320 -height 480
+        ;;
+    CFA-10056)
+		/bin/nice -n -20 /usr/bin/prboom -width 480 -height 800
+        ;;
+    CFA-10057)
+		/usr/bin/nice -n -20 /usr/bin/prboom -width 480 -height 800
+        ;;
+    CFA-10058)
+		/usr/bin/nice -n -20 /usr/bin/prboom -width 800 -height 480
+        ;;
+    esac
+done
+
